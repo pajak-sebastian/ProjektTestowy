b59a75c4a87b991242a3eecaa3dcfd2cb9876acc
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-10-27 09:56:59 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    $conn= new mysqli("127.0.0.1","root","","nauka1");    //adres,login,hasło,login
+    $result=$conn->query('SELECT * FROM pracownicy');
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+
+
+        $conn=new mysqli("127.0.0.1","root","","nauka1");
+        $result=$conn->query('SELECT * FROM pracownicy where zarobki >55');
+            echo("<table border=1>");
+            echo("<th>id</th>");
+            echo("<th>imie</th>");
+            echo("<th>dzial</th>");
+            echo("<th>zarobki</th>");
+                while($row=$result->fetch_assoc()) {
+            echo("<tr>");
+                        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+
+            echo("</tr>");
+                }
+                echo("</table>");
+
+?>
